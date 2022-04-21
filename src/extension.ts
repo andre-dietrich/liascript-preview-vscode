dc2ae125ef3cb5f535c40acb9d95ad719b5df3b3
André Dietrich <andredietrich@web.de>
2022-04-21 11:20:44 +0200

    updated commands

--- src/extension.ts
+++ src/extension.ts
@@ -11,92 +11,80 @@ const PORT = 5622
 export function activate(context: vscode.ExtensionContext) {
   server.init('./node_modules')
 
-  let preview = vscode.commands.registerCommand(
-    'liascript-preview.liascript-preview',
-    () => {
-      const file = vscode.window.activeTextEditor?.document.uri.fsPath?.replace(
-        vscode.workspace.rootPath || '',
-        ''
-      )
-      try {
-        server.run(
-          PORT,
-          'localhost',
-          vscode.workspace.rootPath,
-          undefined,
-          true,
-          false,
-          false
-        )
-        vscode.window.showInformationMessage(
-          `Started LiaScript-DevServer at: http://localhost:${PORT}`
-        )
-      } catch (e: any) {
-        console.log('Server already started: ', e.message)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'liascript-preview.liascript-preview',
+      () => {
+        startPreview(true, false)
       }
-
-      if (file) {
-        open(
-          `http://localhost:${PORT}/liascript/index.html?http://localhost:${PORT}/${file}`
-        )
-      } else {
-        open(`http://localhost:${PORT}`)
-      }
-    }
+    )
   )
 
-  context.subscriptions.push(preview)
-
-  let test = vscode.commands.registerCommand(
-    'liascript-preview.liascript-test',
-    () => {
-      const file = vscode.window.activeTextEditor?.document.uri.fsPath?.replace(
-        vscode.workspace.rootPath || '',
-        ''
-      )
-      try {
-        server.run(
-          PORT,
-          'localhost',
-          vscode.workspace.rootPath,
-          undefined,
-          true,
-          false,
-          true
-        )
-
-        vscode.window.showInformationMessage(
-          `Started LiaScript-DevServer at: http://localhost:${PORT}`
-        )
-      } catch (e: any) {
-        console.log('Server already started: ', e.message)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      'liascript-preview.liascript-preview-live',
+      () => {
+        startPreview(true, true)
       }
-
-      if (file) {
-        open(
-          `https://liascript.github.io/course/?http://localhost:${PORT}/${file}`
-        )
-      } else {
-        open(`http://localhost:${PORT}`)
-      }
-    }
+    )
   )
 
-  context.subscriptions.push(test)
+  context.subscriptions.push(
+    vscode.commands.registerCommand('liascript-preview.liascript-test', () => {
+      startPreview(false, false)
+    })
+  )
 
-
-  let stop = vscode.commands.registerCommand(
-    'liascript-preview.liascript-stop',
-    () => {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('liascript-preview.liascript-stop', () => {
       deactivate()
-    }
+    })
   )
-
-  context.subscriptions.push(stop)
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
   server.stop()
   vscode.window.showInformationMessage(`LiaScript-DevServer terminated`)
+}
+
+function startPreview(previewMode: boolean, liveMode: boolean) {
+  const file = vscode.window.activeTextEditor?.document.uri.fsPath?.replace(
+    vscode.workspace.rootPath || '',
+    ''
+  )
+
+  try {
+    server.run(
+      PORT,
+      'localhost',
+      vscode.workspace.rootPath,
+      undefined,
+      liveMode,
+      false,
+      false
+    )
+
+    vscode.window.showInformationMessage(
+      `Started LiaScript-DevServer ${
+        liveMode ? '(in live mode)' : ''
+      } at: http://localhost:${PORT}`
+    )
+  } catch (e: any) {
+    console.log('Server already started: ', e.message)
+  }
+
+  if (file) {
+    if (previewMode) {
+      open(
+        `http://localhost:${PORT}/liascript/index.html?http://localhost:${PORT}/${file}`
+      )
+    } else {
+      open(
+        `https://liascript.github.io/course/?http://localhost:${PORT}/${file}`
+      )
+    }
+  } else {
+    open(`http://localhost:${PORT}`)
+  }
 }
